6984dae5e9ef8b134c6775bd21ee45ae6b007640
Kerrie Batrouney <batrouney@gmail.com>
2016-09-28 15:57:29 +1000

    3 objects exercises

--- Kerrie/day2/index.js
+++ Kerrie/day2/index.js
@@ -3,11 +3,16 @@ var name = prompt("Your name please?");
 
 console.log("So your name is "+name+ "?");
 
-for (var i = 1; i < 11; i++) {
-  console.log(i + ' * 9 = ' +i*9);
+for (var i = 1; i <= 10; i++) {
+//  console.log(i + ' * 9 = ' +i*9);
+
+//bonus
+for (var j = 1; j<=10; j++); {
+  console.log(j + ' times ' + i + ' = ' + (i*j));
+}
 }
 
-console.log('sorry, I do not understand bonus requirement');
+//console.log('sorry, I do not understand bonus requirement');
 
 
 console.log("On the menu tonight: Steak, fruit salad, tofurkey, pork chops");
@@ -24,4 +29,13 @@ if ((selection)===(onTheMenuTonight[0])) {
   console.log('Vegans beware');
 } else {
   console.log('Please only select from the 4 items');
+}
+var choice = 'steak';
+
+if (choice === 'steak' || choice ==='pork chops') {
+  console.log('meat');
+} else {
+
+
+var choice = 'steak';
 }
